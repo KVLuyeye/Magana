35edaddc2526588b229d10568334182c30a5f4e0
KVLuyeye <josueluyeyejkl@gmail.com>
2024-05-16 14:17:27 +0200

    change backend to server

--- client/src/components/Nav/NavBar.vue
+++ client/src/components/Nav/NavBar.vue
@@ -2,7 +2,7 @@
 import NavItem from './NavItem.vue';
 import HomeIcon from '@/assets/icons/HomeIcon.vue';
 import ExchangeIcon from '@/assets/icons/ExchangeIcon.vue';
-import AccountIcon from '@/assets/icons/AccountIcon.vue';
+// import AccountIcon from '@/assets/icons/AccountIcon.vue';
 import TransactionIcon from '@/assets/icons/TransactionIcon.vue';
 import BellIcon from '@/assets/icons/BellIcon.vue';
 
@@ -18,16 +18,15 @@ const isActive = (path: string) => {
 
 <template>
   <nav
-    class="flex h-[4em] flex-row items-center justify-between rounded-tl-lg rounded-tr-lg border border-b-0 border-l-0 border-r-0 border-t-2 border-solid border-[#136207] bg-white pb-8 pl-4 pr-4"
-  >
+    class="flex h-[4em] flex-row items-center justify-between rounded-tl-lg rounded-tr-lg border border-b-0 border-l-0 border-r-0 border-t-2 border-solid border-[#136207] bg-white pb-8 pl-4 pr-4">
     <NavItem to="/home" :active="isActive('/home')">
       <HomeIcon :class="{ 'active-icon': isActive('/home') }" />
     </NavItem>
     <NavItem to="/transfer" :active="isActive('/transfer')">
       <TransactionIcon :class="{ 'active-icon': isActive('/transfer') }" />
     </NavItem>
-    <NavItem to="/accounts" :active="isActive('/accounts')">
+    <!-- <NavItem to="/accounts" :active="isActive('/accounts')">
       <AccountIcon :class="{ 'active-icon': isActive('/accounts') }" />
-    </NavItem>
+    </NavItem> -->
   </nav>
 </template>
